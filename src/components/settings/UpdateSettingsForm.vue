6f64e9b8b22b96dbd4e1661f0aa44324324988f1
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2023-09-22 20:39:04 +0700

    fixed update setting change number

--- src/components/settings/UpdateSettingsForm.vue
+++ src/components/settings/UpdateSettingsForm.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { ref, watch } from 'vue';
 import AppForm from '@/ui/AppForm.vue';
 import AppFormRow from '@/ui/AppFormRow.vue';
 import AppInput from '@/ui/AppInput.vue';
@@ -12,6 +13,8 @@ const {
   settings
 } = useSetting(1)
 
+const setting = ref<SettingResponse | null>(null)
+
 const { isUpdating, updateSetting } = useUpdateSetting()
 
 function handleUpdate(e: Event, field: Setting) {
@@ -21,13 +24,25 @@ function handleUpdate(e: Event, field: Setting) {
     return
   }
 
-  const setting = { ...settings.value, [field]: Number(value) } as SettingResponse
+  setting.value = { ...setting.value, [field]: Number(value) } as SettingResponse
 
   updateSetting({
-    setting,
+    setting: setting.value,
     id: 1
   })
 }
+
+watch(
+  settings,
+  (newSetting) => {
+    if (newSetting) {
+      setting.value = newSetting
+    }
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
 <template>
@@ -35,22 +50,22 @@ function handleUpdate(e: Event, field: Setting) {
 
   <AppForm v-else>
     <AppFormRow label="Minimum nights/booking">
-      <AppInput type="number" id="min-nights" :model-value="settings?.min_booking_length"
+      <AppInput type="number" id="min-nights" :model-value="setting?.min_booking_length"
         @blur="handleUpdate($event, 'min_booking_length')" :disabled="isUpdating" />
     </AppFormRow>
 
     <AppFormRow label="maximum nights/booking">
-      <AppInput type="number" id="max-nights" :model-value="settings?.max_booking_length"
+      <AppInput type="number" id="max-nights" :model-value="setting?.max_booking_length"
         @blur="handleUpdate($event, 'max_booking_length')" :disabled="isUpdating" />
     </AppFormRow>
 
     <AppFormRow label="maximum guests/booking">
-      <AppInput type="number" id="max-guests" :model-value="settings?.max_guest_per_booking"
+      <AppInput type="number" id="max-guests" :model-value="setting?.max_guest_per_booking"
         @blur="handleUpdate($event, 'max_guest_per_booking')" :disabled="isUpdating" />
     </AppFormRow>
 
     <AppFormRow label="Breakfast price">
-      <AppInput type="number" id="breakfast-price" :model-value="settings?.breakfast_price"
+      <AppInput type="number" id="breakfast-price" :model-value="setting?.breakfast_price"
         @blur="handleUpdate($event, 'breakfast_price')" :disabled="isUpdating" />
     </AppFormRow>
   </AppForm>
